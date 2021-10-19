462f199b7632ca4970c00d9ef7f4c5c2ce96a451
andreahenos <henostroza.andrea@gmail.com>
2021-10-19 16:52:52 -0500

    Primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
-};
+encode: (offset, str) => {
+    offset = parseInt(offset)
+    let result = "";
+    for(let position = 0; position<str.length; position++){
+        let codeOfChar = str.charCodeAt(position);
+        console.log(codeOfChar)
+    let newPosition = (codeOfChar -65 + offset) %26 +65;
+    console.log(newPosition);
+    let newLetter = String.fromCharCode(newPosition);
+    console.log(newLetter);
+    result += newLetter
+    console.log(result)
+    }
+return result;
+},
+
+decode: (offset, str) => {
+    offset = parseInt(offset)
+    let result = "";
+    for(let position = 0; position<str.length; position++){
+        let codeOfChar = str.charCodeAt(position);
+        console.log(codeOfChar);
+        let newPosition = (codeOfChar -65 +26 -(offset %26)) %26 +65;
+        console.log(newPosition);
+        let newLetter = String.fromCharCode(newPosition);
+        console.log(newLetter);
+        result += newLetter
+        console.log(result)
+    }
+return result;
+}
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+//Botón cifrar
+document.getElementById("encodeButton").addEventListener("click", function(){
+  let offset = document.getElementById("inputOffset").value;
+  let str = document.getElementById("inputText").value;
+  let strConv = document.getElementById("outputText").value;
+  strConv = cipher.encode(offset, str);
+});
+
+//Botón descifrar
+document.getElementById("decodeButton").addEventListener("click", function(){
+  let offset = document.getElementById("inputOffset").value;
+  let str = document.getElementById("inputText").value;
+  let strConv = document.getElementById("outputText").value;
+  strConv = cipher.decode(offset, str);
+}); 
+
+//Botón copiar
+document.getElementById("copyButton").addEventListener("click", copy);
+function copy() {
+  alert("Funciona copiar");
+} 
+
+//Botón borrar
+document.getElementById("eraseButton").addEventListener("click", erase);
+function erase() {
+  document.getElementById("inputText").value = "";
+  document.getElementById("outputText").value = "";
+}
+
+
